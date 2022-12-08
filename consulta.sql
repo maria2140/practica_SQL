8bdf5af7c5dea499f59d13b1c5b2a9c26052378c
maria2140 <67831016+maria2140@users.noreply.github.com>
2022-12-08 18:16:27 +0100

    changed name

--- consulta.sql
+++ consulta.sql
@@ -9,13 +9,10 @@ c.total_km as "Kilómetros totales",
 a.aseguradora as "Aseguradora",
 c.num_poliza as "Número de póliza"
 
+from modelado_maria_ojie.coches c 
 
-from modelado_maria_ojie_fayos.coches c 
-
-
-
-inner join modelado_maria_ojie_fayos.modelos mo on c.id_modelo = mo.id_modelo
-inner join modelado_maria_ojie_fayos.marcas m on c.id_marca = m.id_marca
-inner join modelado_maria_ojie_fayos.grupomarca g on c.id_grupo_marca = g.id_grupo_marca
-inner join modelado_maria_ojie_fayos.colores c2 on c.id_color = c2.id_color
-inner join modelado_maria_ojie_fayos.aseguradoras a on c.id_aseguradora = a.id_aseguradora
+inner join modelado_maria_ojie.modelos mo on c.id_modelo = mo.id_modelo
+inner join modelado_maria_ojie.marcas m on c.id_marca = m.id_marca
+inner join modelado_maria_ojie.grupomarca g on c.id_grupo_marca = g.id_grupo_marca
+inner join modelado_maria_ojie.colores c2 on c.id_color = c2.id_color
+inner join modelado_maria_ojie.aseguradoras a on c.id_aseguradora = a.id_aseguradora
